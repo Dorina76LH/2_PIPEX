e31e9c225fc3f1f66f2bbb7875fad5e098002c22
Dorina76LH <dorina.beres@gmail.com>
2025-04-28 17:52:26 +0200

    Update 1_error.c

--- source/1_error.c
+++ source/1_error.c
@@ -10,6 +10,19 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+/**
+	@file 1_error.c
+	@brief Contains error handling functions for Pipex.
+
+	This file includes:
+	- The `error` function to handle and display error messages
+	  and terminate the program.
+	
+	@note The `error` function is used throughout the program to manage
+	exceptional situations that prevent further execution. It ensures
+	that a descriptive error message is displayed, and the program exits gracefully.
+*/
+
 // =========================================================================
 // -------------------------- Included libraries ---------------------------
 // =========================================================================
@@ -19,6 +32,17 @@
 // =========================================================================
 // --------------------------------- error ---------------------------------
 // =========================================================================
+
+/**
+	error - Display an error message and terminate the program.
+
+	@note This function uses `perror` to print the given error message 
+	and then exits the program with a failure status.
+
+	@param msg The error message to be displayed.
+
+	@return None. The program is terminated after this function is called.
+*/
 int	error(char *msg)
 {
 	perror(msg);
